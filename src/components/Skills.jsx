7af35d1ac7f4670f6853acd00d97ec5ff5da9cb7
TanyaCoder-27 <tanyakonapala@gmail.com>
2025-03-17 22:21:11 +0530

    skills styling done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,3 @@
-import { FaCheckCircle } from "react-icons/fa";
-
 const skills = {
   technical: [
     "Frontend Development: HTML, CSS, JavaScript, React, Tailwind CSS, Bootstrap, Firebase",
@@ -22,20 +20,16 @@ const Skills = () => {
     <div className="bg-black text-white py-20" id="skills">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">My Skills</h2>
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Technical Skills */}
           <div className="bg-gray-800 px-6 py-6 rounded-lg hover:shadow-lg transition-transform transform hover:scale-105">
             <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-4">
               Technical Skills
             </h3>
-            <ul className="text-gray-300 space-y-2">
+            <ul className="text-gray-300 list-disc list-inside">
               {skills.technical.map((skill, index) => (
-                <li
-                  key={index}
-                  className="flex items-center space-x-2 hover:text-green-400 transition-colors"
-                >
-                  <FaCheckCircle className="text-green-400" />
-                  <span>{skill}</span>
+                <li key={index} className="hover:text-green-400 transition-colors">
+                  {skill}
                 </li>
               ))}
             </ul>
@@ -45,14 +39,10 @@ const Skills = () => {
             <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-4">
               Non-Technical Skills
             </h3>
-            <ul className="text-gray-300 space-y-2">
+            <ul className="text-gray-300 list-disc list-inside">
               {skills.nonTechnical.map((skill, index) => (
-                <li
-                  key={index}
-                  className="flex items-center space-x-2 hover:text-blue-400 transition-colors"
-                >
-                  <FaCheckCircle className="text-blue-400" />
-                  <span>{skill}</span>
+                <li key={index} className="hover:text-blue-400 transition-colors">
+                  {skill}
                 </li>
               ))}
             </ul>
