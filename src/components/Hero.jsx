82060d27280370688b759bcfb84ea14716b542b9
TanyaCoder-27 <tanyakonapala@gmail.com>
2026-04-06 00:07:57 +0530

    mimimal changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,9 @@ const Hero = ({ contactRef }) => {
   return (
     <div className='bg-black text-white text-center py-16'>
       <img src={HeroImage} alt="" className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105'/>
-      <h1 className='text-4xl font-bold'>I am <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Andra Tulasi Lakshmi Tanya</span>, passionate Computer Science student with a focus on building modern and responsive web applications.</h1>
+      <h1 className='text-4xl font-bold'>I am <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Andra Tulasi Lakshmi Tanya</span>, passionate Computer Science student with a focus on building modern, responsive web applications and advancing automation testing.
+
+</h1>
       <p className='mt-4 text-lg text-gray-300'>I am interested in Automation Testing,Web development and GenAI.</p>
       <div className='mt-8 space-x-4'>
         <button onClick={scrollToContact} className='bg-gradient-to-r from-green-400 to-blue-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button>
